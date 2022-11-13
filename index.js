d7004c7ea0c9da31488c7538f9fcc87d432de7f7
Darwin Rojas V <dwnrojasvalencia@outlook.com>
2022-11-13 08:06:42 -0500

    Se  añadió el carritoService y el index

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const body_parse = require("body-parse")
+const path = require("path");
+const carritoService = require("./carritoService.js");
+
+const app = express();
+const port = 8084;
+
+app.use(cors());
+app.use(body_parse.json());
+
+const pathName = "/"
+
+
+
+
+
+app.listen(port,
+    ()=>{
+        console.log("Se subio la app carrito en el puerto" + port);
+    }
+    );
